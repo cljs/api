cd743f0d1503f039a55558661150bb432f1ea678
Shaun Williams <shaun.e.williams@gmail.com>
2015-05-05 01:13:37 -0500

    add travis

---
+++ script/travis-publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ev
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  cd output-repo
+  git config user.name "${GIT_NAME}"
+  git config user.email "${GIT_EMAIL}"
+  git remote add origin https://github.com/cljsinfo/api-docs-generator.git
+  echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+  git push --force --tags origin docs
+fi
+
