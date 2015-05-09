88851b4efd0357d2089629ab02b44c5c22494284
Shaun Williams <shaun.e.williams@gmail.com>
2015-05-09 14:48:26 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject get-symbol-metadata "0.1.0-SNAPSHOT"
+(defproject cljs-api-gen "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/tools.reader "0.8.12"]
                  [org.clojure/core.match "0.2.2"]
-                 [enlive "1.1.1"]
                  [me.raynes/fs "1.4.6"]]
   :main cljs-api-gen.core)
