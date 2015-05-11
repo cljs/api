5af86820a1bfceab6cabc96fa2ad4ca3a5da002b
Shaun Williams <shaun.e.williams@gmail.com>
2015-05-11 06:22:55 -0500

    add better ns reader

--- src/cljs_api_gen/read.clj
+++ src/cljs_api_gen/read.clj
@@ -5,7 +5,13 @@
     [clojure.tools.reader.reader-types :as readers]
     [cljs.tagged-literals :refer [*cljs-data-readers*]]
     [me.raynes.fs :refer [exists?]]
+    [cljs-api-gen.config :refer [repo-dir
+                                 *cljs-num*]]
     ))
+
+;;--------------------------------------------------------------------------------
+;; Read forms
+;;--------------------------------------------------------------------------------
 
 (defn read-forms
   [r]
@@ -30,4 +36,44 @@
   [s]
   (let [r (readers/string-push-back-reader s)]
     (read-forms r)))
+
+;;--------------------------------------------------------------------------------
+;; Read forms from namespaces
+;;--------------------------------------------------------------------------------
+
+(defmulti src-path
+  identity)
+
+(defmethod src-path :library
+  [src-type]
+  (cond
+    (=> *cljs-num* 3255) "src/main/cljs"
+    (=> *cljs-num* 0)    "src/cljs"
+    :else nil))
+
+(defmethod src-path :compiler
+  [src-type]
+  (cond
+    (=> *cljs-num* 3255) "src/main/clojure"
+    (=> *cljs-num* 0)    "src/clj"
+    :else nil))
+
+(defn get-ns-files
+  [ns- src-types]
+  (filter exists?
+    (let [ns-path (replace ns- "." "/")]
+      (for [src (map src-path src-types)
+            ext  ["clj" "cljs" "cljc"]]
+        (str repo-dir "/clojurescript/" src "/" ns-path "." ext)))))
+
+(defn read-ns-forms
+  [ns- src-types]
+  (->> (get-ns-files ns- src-types)
+       (mapcat read-forms-from-file)))
+
+(defn read-clj-core-forms
+  []
+  (->> ["core" "core_proxy" "core_print" "core_deftype"]
+       (map #(str repo-dir "/clojure/src/clj/clojure/" % ".clj"))
+       (mapcat read-forms-from-file)))
 
