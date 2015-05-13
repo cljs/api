8b20f46bc151ff02fb65521ed7efd0396a462115
Shaun Williams <shaun.e.williams@gmail.com>
2015-05-12 20:44:32 -0500

    update travis script

--- script/travis-publish.sh
+++ script/travis-publish.sh
@@ -6,8 +6,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   cd output-repo
   git config user.name "${GIT_NAME}"
   git config user.email "${GIT_EMAIL}"
-  git remote add origin https://github.com/cljsinfo/api-docs-generator.git
-  echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-  git push --force --tags origin docs
+  git remote add origin https://shaunlebron:${GH_TOKEN}@github.com/cljsinfo/api-docs-generator.git
+  git push --force --tags origin docs &> /dev/null
 fi
 
