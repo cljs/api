30dcc1d9c9cf739ca02b976ec1100da1c68879b7
Shaun Williams <shaun.e.williams@gmail.com>
2015-05-12 20:56:19 -0500

    correct catalog directory in travis script

--- script/travis-publish.sh
+++ script/travis-publish.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  cd output-repo
+  cd catalog
   git config user.name "${GIT_NAME}"
   git config user.email "${GIT_EMAIL}"
   git remote add origin https://shaunlebron:${GH_TOKEN}@github.com/cljsinfo/api-docs-generator.git
