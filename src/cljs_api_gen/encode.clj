a21d96dd280f8abb015b33b511decab3fb1eab1f
Shaun Williams <shaun.e.williams@gmail.com>
2015-06-02 11:51:05 -0500

    add encoding for future syntax docs

--- src/cljs_api_gen/encode.clj
+++ src/cljs_api_gen/encode.clj
@@ -3,16 +3,33 @@
   (:require
     [clojure.string :refer [replace]]))
 
+;; FIXME: if encoding/decoding fails to be lossless, and we need to add another underscore to each side
+;;        create a encoding name decorator function #(str "__" % "__")
 (def encoding
-  {"." "_DOT_"
-   ">" "_GT_"
-   "<" "_LT_"
-   "!" "_BANG_"
-   "?" "_QMARK_"
-   "*" "_STAR_"
-   "+" "_PLUS_"
-   "=" "_EQ_"
-   "/" "_SLASH_"})
+  {"."  "_DOT_"
+   ">"  "_GT_"
+   "<"  "_LT_"
+   "!"  "_BANG_"
+   "?"  "_QMARK_"
+   "*"  "_STAR_"
+   "+"  "_PLUS_"
+   "="  "_EQ_"
+   "/"  "_SLASH_"
+
+   ";"  "_SEMICOLON_" ;; syntax - comment
+   "\\" "_BSLASH_"    ;; syntax - character
+   "#"  "_HASH_"      ;; syntax - tagged literal
+   "["  "_LSQUARE_"   ;; syntax - vector
+   "]"  "_RSQUARE_"
+   "{"  "_LCURLY_"    ;; syntax - map
+   "}"  "_RCURLY_"
+   "("  "_LPAREN_"    ;; syntax - list
+   ")"  "_RPAREN_"
+   "^"  "_CARET_"     ;; syntax - meta
+   "\"" "_DQUOTE_"    ;; syntax - string
+   "'"  "_SQUOTE_"    ;; syntax - quote
+   "@"  "_ATSIGN_"    ;; syntax - reader-conditional splice
+   })
 
 (defn encode-name [name-]
   (reduce (fn [s [a b]] (replace s a b))
