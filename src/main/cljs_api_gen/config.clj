2ef51fb918a97b5805d24a8f68488241e56ef5af
Shaun Williams <shaun.e.williams@gmail.com>
2016-06-12 01:29:13 -0500

    remove unused in config

--- src/main/cljs_api_gen/config.clj
+++ src/main/cljs_api_gen/config.clj
@@ -1,18 +1,8 @@
 (ns cljs-api-gen.config)
 
 (def cache-dir "cache")
-
-;; location of manual cljsdoc files
 (def cljsdoc-dir "cljsdoc")
-
-;; cache directory
-(defn edn-parsed-file [v] (str v ".edn"))
-(defn edn-result-file [v] (str "cljs-api.edn"))
-
-;; location of the clojure & clojurescript repos to parse
 (def repos-dir "repos")
 
-(def refs-dir "refs")
-
-;; doc page generated for the cljs website
-(def site-dir "site")
+(defn edn-parsed-file [v] (str v ".edn"))
+(defn edn-result-file [v] (str "cljs-api.edn"))
