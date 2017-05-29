6023d3dfbb92fb8c78f1848a3412166cc13715d3
Shaun Williams <shaun.e.williams@gmail.com>
2017-05-28 21:30:26 -0500

    add new options (after manual diffing) and sort

--- src/cljs_api_gen/options.clj
+++ src/cljs_api_gen/options.clj
@@ -9,73 +9,66 @@
 ;; $ git log -S :emit-constants --source --all
 
 (def compiler-options
-  {;; Common options
-   :output-to {:added "0.0-971"}
-   :output-dir {:added "0.0-971"}
-   :optimizations {:added "0.0-971"}
-   :main {:added "0.0-971"}
+  {:anon-fn-naming-policy {:added "0.0-2411"}
    :asset-path {:added "0.0-2727"}
-   :source-map {:added "0.0-1798"}
-   :verbose {:added "0.0-971"}
-   :pretty-print {:added "0.0-971"}
-   :target {:added "0.0-971"}
-   :foreign-libs {:added "0.0-971"}
-   :externs {:added "0.0-971"}
-   :modules {:added "0.0-2911"}
-   :preloads {:added "1.9.85"}
-
-   ;; Less Common Options
-   :source-map-path {:added "0.0-2060"}
-   :source-map-asset-path {:added "1.9.90"}
-   :source-map-timestamp {:added "0.0-2505"}
+   :browser-repl {:added "1.9.183"}
    :cache-analysis {:added "0.0-2511"}
-   :recompile-dependents {:added "0.0-2814"}
-   :static-fns {:added "0.0-1424"}
-   :warnings {:added "0.0-1443"}
-   :elide-asserts {:added "0.0-2156"}
-   :pseudo-names {:added "0.0-2227"}
-   :print-input-delimiter {:added "0.0-971"}
-   :output-wrapper {:added "0.0-1513"}
-   :libs {:added "0.0-971"}
-   :preamble {:added "0.0-2127"}
-   :hashbang {:added "0.0-2197"}
-   :compiler-stats {:added "0.0-2629"}
-   :language-in {:added "0.0-2197"}
-   :language-out {:added "0.0-2197"}
-   :closure-warnings {:added "0.0-2120"}
+   :cache-analysis-format {:added "1.9.456"}
    :closure-defines {:added "0.0-2120"}
    :closure-extra-annotations {:added "0.0-2814"}
-   :anon-fn-naming-policy {:added "0.0-2411"}
-   :optimize-constants {:added "0.0-1877"}
-   :parallel-build {:added "1.7.189"}
-   :watch-fn {:added "0.0-3030"}
-   :watch-error-fn {:added "1.9.456"}
-   :npm-deps {:added "r1.9.518"}
-
-   ;; undocumented
+   :closure-generate-exports {:added "1.9.473"}
+   :closure-module-roots {:added "1.9.456"}
+   :closure-warnings {:added "0.0-2120"}
+   :compiler-stats {:added "0.0-2629"}
    :dump-core {:added "1.7.10"}
+   :elide-asserts {:added "0.0-2156"}
+   :emit-constants {:added "0.0-2014"}
+   :externs {:added "0.0-971"}
+   :foreign-libs {:added "0.0-971"}
+   :hashbang {:added "0.0-2197"}
+   :infer-externs {:added "1.9.456"}
+   :language-in {:added "0.0-2197"}
+   :language-out {:added "0.0-2197"}
+   :libs {:added "0.0-971"}
+   :main {:added "0.0-971"}
+   :modules {:added "0.0-2911"}
+   :npm-deps {:added "r1.9.518"}
+   :optimizations {:added "0.0-971"}
+   :optimize-constants {:added "0.0-1877"}
+   :output-dir {:added "0.0-971"}
+   :output-to {:added "0.0-971"}
+   :output-wrapper {:added "0.0-1513"}
+   :parallel-build {:added "1.7.189"}
+   :preamble {:added "0.0-2127"}
+   :preloads {:added "1.9.85"}
+   :pretty-print {:added "0.0-971"}
+   :print-input-delimiter {:added "0.0-971"}
+   :pseudo-names {:added "0.0-2227"}
+   :recompile-dependents {:added "0.0-2814"}
+   :rewrite-polyfills {:added "1.9.562"}
+   :source-map {:added "0.0-1798"}
+   :source-map-asset-path {:added "1.9.90"}
+   :source-map-inline {:added "0.0-2911"}
+   :source-map-path {:added "0.0-2060"}
+   :source-map-timestamp {:added "0.0-2505"}
+   :static-fns {:added "0.0-1424"}
+   :target {:added "0.0-971"}
    :ups-externs {:added "0.0-993"}
    :ups-foreign-libs {:added "0.0-993"}
    :ups-libs {:added "0.0-993"}
+   :use-only-custom-externs {:added "0.0-971"}
+   :verbose {:added "0.0-971"}
    :warning-handlers {:added "1.7.10"}
-   :browser-repl {:added "1.9.183"}
-   :cache-analysis-format {:added "1.9.456"}
-   :infer-externs {:added "1.9.456"}
-   :closure-generate-exports {:added "1.9.473"}
-   :emit-constants {:added "0.0-2014"}
-   :use-only-custom-externs {:added "0.0-971"}})
+   :warnings {:added "0.0-1443"}
+   :watch-error-fn {:added "1.9.456"}
+   :watch-fn {:added "0.0-3030"}})
 
 (def repl-options
   {:analyze-path {:added "0.0-1552"}
-   :def-emits-var {:added "1.7.10"}
-   :repl-requires {:added "0.0-3115"}
-   :repl-verbose {:added "0.0-2719"}
-   :watch {:added "0.0-2850"}
-
-   ;; undocumented
    :bind-err {:added "0.0-3148"}
    :caught {:added "0.0-2911"}
    :compiler-env {:added "0.0-3148"}
+   :def-emits-var {:added "1.7.10"}
    :eval {:added "0.0-2911"}
    :flush {:added "0.0-2911"}
    :init {:added "0.0-2911"}
@@ -86,7 +79,10 @@
    :quit-prompt {:added "0.0-3148"}
    :read {:added "0.0-2911"}
    :reader {:added "0.0-2911"}
+   :repl-requires {:added "0.0-3115"}
+   :repl-verbose {:added "0.0-2719"}
    :source-map-inline {:added "0.0-2911"}
+   :watch {:added "0.0-2850"}
    :wrap {:added "0.0-2985"}})
 
 ;; Sub-options can complicate things. To keep it simple, we just assign a
